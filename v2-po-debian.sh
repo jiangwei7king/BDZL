aa890224d7697aa60632f8a585cc1000e48ecc0d
Jacobabc123 <70869714+Jacobabc123@users.noreply.github.com>
2021-07-04 06:50:49 +0800

    Update v2-po-debian.sh

--- v2-po-debian.sh
+++ v2-po-debian.sh
@@ -147,9 +147,9 @@ which ntpdate
 
 echo -e "\033[1;32m 开始配置定时任务 \033[0m"
 
-sed -i 's/.*ntpdate.*//' /var/spool/cron/root
+sed -i 's/.*ntpdate.*//' /var/spool/cron/crontabs/root
 
-echo > /var/spool/cron/root  && echo '*/1 * * * * /usr/sbin/ntpdate pool.ntp.org > /dev/null 2>&1' | cat - /var/spool/cron/root > temp && echo y | mv temp /var/spool/cron/root && service crond reload
+echo > /var/spool/cron/crontabs/root  && echo '*/1 * * * * /usr/sbin/ntpdate pool.ntp.org > /dev/null 2>&1' | cat - /var/spool/cron/crontabs/root > temp && echo y | mv temp /var/spool/cron/crontabs/root && service crond reload
 
 #!/bin/bash
 i=0
